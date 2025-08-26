8e8af57aa9c17fe7c6dff03c2fb04492033f498d
jnnnkjbbbbbbbbbbbbbbbb <estigoyjz2028@wvc.pshs.edu.ph>
2025-08-26 12:55:26 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hello, GitHub!")
+git remote add origin https://github.com/jnnnkjbbbbbbbbbbbbbbbb/CS_Elective.git
+git branch -M main
+git push -u origin main
+print("Hello, GitHub! Now this is version 2.")
