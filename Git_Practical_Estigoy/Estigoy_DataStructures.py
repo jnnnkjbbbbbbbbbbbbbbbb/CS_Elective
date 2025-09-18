e8206a19380aa1faee17e14861f0377226d36aad
jnnnkjbbbbbbbbbbbbbbbb <estigoyjz2028@wvc.pshs.edu.ph>
2025-09-18 14:22:48 +0800

    initial commit

---
+++ Git_Practical_Estigoy/Estigoy_DataStructures.py
@@ -0,0 +1,22 @@
+students =["jaylene","mraye","adi","johnica","janel"]
+name=("jaylene","mraye","adi","johnica","janel")
+age=(14,15,16,15,16)
+gradelevel=(10,10,10,10,10)
+section=("Photon","Graviton","Tau","Photon","Electron")
+print(name[2])
+print(gradelevel[2])
+print(section[2])
+grades={
+    "jaylene": 97,
+    "mraye": 96,
+    "adi": 95,
+    "johnica": 96,
+    "janel": 95,
+
+
+}
+print(grades["jaylene"])
+grades["jaylene"]=90
+print(grades["jaylene"])
+name.append("sofia",100)
+print("sofia",name)
